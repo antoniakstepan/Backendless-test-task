9ba53c26b6cd44e9507dd0e66bc7558876e5c834
Antoniak Stepan <antoniakstepan1@gmail.com>
2023-10-26 12:07:13 +0300

    Add tamplate

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Link, Route, Routes, useLocation, Outlet } from 'react-router-dom';
+import data from '../data/tabs.json';
+
+const App = () => {
+  const location = useLocation();
+  // Set the default tab if the URL doesn't have a tab specified
+  useEffect(() => {
+    if (!location.pathname || location.pathname === '/') {
+      // Redirect to the default tab
+      window.location.href = `/tab/${data.defaultTab}`;
+    }
+  }, [location.pathname]);
+
+  return (
+    <div>
+      <nav>
+        {data.tabs.map((tab) => (
+          <Link key={tab.id} to={`/tab/${tab.id}`}>
+            {tab.title}
+          </Link>
+        ))}
+      </nav>
+      <hr />
+      <Routes>
+        {data.tabs.map((tab) => {
+          console.log("tab", tab)
+          return (
+            <Route key={tab.id} path={`/tab/${tab.id}`} element={<TabLoader tab={tab} />} />
+          )
+        })}
+      </Routes>
+    </div>
+  );
+};
+
+const TabLoader = ({ tab }) => {
+  // Dynamically import the component from the specified path
+  const ComponentToLoad = React.lazy(() => import(`${tab.componentPath}`));
+
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Outlet />
+      <ComponentToLoad />
+    </React.Suspense>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
